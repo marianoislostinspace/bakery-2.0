c3637cfc24f495a299720f2eb48dde9916a6045e
Alexis Acevedo <izumikeiper@gmail.com>
2024-04-23 18:11:37 -0300

    Me cansé

---
+++ bread.js
@@ -0,0 +1,44 @@
+let baguette = document.getElementById("baguette");
+
+baguette.addEventListener("click", function () {
+    
+    baguette.textContent = 'La baguette, un clásico de la panadería francesa, es un pan de masa fermentada de forma alargada y crujiente cuya historia se remonta al siglo XVIII. Con una corteza dorada y crujiente y una miga suave y aireada, la baguette es apreciada por su sabor sutil y versatilidad en la cocina. Ya sea acompañada de mantequilla y mermelada para el desayuno, rellena con ingredientes frescos para un sándwich ligero o como acompañamiento perfecto para una tabla de quesos, la baguette es un elemento esencial en la mesa de cualquier amante del pan'
+    baguette.style.fontSize = '30px'
+})
+
+
+let criollo = document.getElementById("criollo");
+
+criollo.addEventListener("click", function () {
+    criollo.textContent = 'El pan criollo, un tesoro de la cocina latinoamericana, es un pan tradicionalmente elaborado con ingredientes simples pero llenos de sabor. Con una miga suave y esponjosa y una corteza dorada y crujiente, el pan criollo cautiva los sentidos con su aroma tentador y su delicioso sabor casero. Perfecto para acompañar cualquier comida, desde desayunos reconfortantes hasta cenas familiares, este pan versátil es un elemento básico en la mesa de muchos hogares latinoamericanos.'
+    criollo.style.fontSize = '30px'
+})
+
+let lactal = document.getElementById("lactal");
+
+lactal.addEventListener("click", function () {
+    lactal.textContent = 'El pan lactal de molde, conocido también como pan de sandwich o pan de molde, es un clásico de la panadería que se distingue por su forma rectangular y su textura suave y uniforme. Elaborado con una mezcla de harina, agua, levadura, sal, azúcar y a menudo una pequeña cantidad de grasa, este pan se caracteriza por su versatilidad y su capacidad para complementar una amplia variedad de comidas. Desde sándwiches simples hasta tostadas para el desayuno o incluso como base para recetas más elaboradas, el pan lactal de molde es una opción confiable y reconfortante en cualquier ocasión.'
+    lactal.style.fontSize = '30px'
+})
+
+let integral = document.getElementById("integral");
+
+integral.addEventListener("click", function () {
+    integral.textContent = 'El pan integral es una delicia saludable y nutritiva que se distingue por su color oscuro y su textura densa y nutritiva. Elaborado con harina integral, que conserva el salvado y el germen del grano de trigo, este pan ofrece una fuente rica en fibra, vitaminas y minerales esenciales para una dieta equilibrada. Su sabor robusto y ligeramente dulce lo convierte en una opción deliciosa para aquellos que buscan cuidar su salud sin renunciar al placer de comer pan. Desde sándwiches nutritivos hasta acompañamientos para comidas saludables, el pan integral es una elección versátil y satisfactoria para cualquier ocasión.hola'
+    integral.style.fontSize = '30px'
+})
+
+let panetone = document.getElementById("panetone");
+
+panetone.addEventListener("click", function () {
+    panetone.textContent = 'El panettone, un clásico de la repostería italiana, es un pan dulce de forma cilíndrica y textura esponjosa, enriquecido con frutas confitadas y frutos secos. Esta delicia navideña tiene sus raíces en la región de Milán, donde se elabora tradicionalmente durante las festividades de fin de año. Su aroma embriagador a cítricos y vainilla, junto con su sabor dulce y ligeramente almibarado, lo convierten en un manjar irresistible durante la temporada navideña. Cada bocado de panettone es una experiencia única que evoca la calidez y la alegría de la temporada, y su presentación decorativa lo convierte en un regalo popular para compartir con seres queridos durante las celebraciones festivas.'
+    panetone.style.fontSize = '27px'
+})
+
+let ciabatta = document.getElementById("ciabatta");
+
+ciabatta.addEventListener("click", function () {
+    ciabatta.textContent = 'La ciabatta es un pan italiano conocido por su forma alargada y su miga alveolada y aireada. Este pan se caracteriza por su corteza crujiente y su interior suave y esponjoso, lo que lo convierte en un acompañamiento delicioso para una variedad de platos. La ciabatta es especialmente apreciada por su versatilidad, ya que es perfecta para hacer sándwiches, acompañar sopas o simplemente disfrutarla con un poco de aceite de oliva y vinagre balsámico. Con su sabor sutil y su textura distintiva, la ciabatta es un elemento básico en la cocina italiana y una delicia para los amantes del pan en todo el mundo.'
+    ciabatta.style.fontSize = '30px'
+})
+
---
+++ sausage.js
@@ -0,0 +1,64 @@
+let jamon = document.getElementById("jamon");
+
+jamon.addEventListener("click", function () {
+    jamon.textContent = 'Encuentra en la suavidad y el sabor delicado de nuestro Jamón Cocido Premium, una opción clásica y versátil que deleita los sentidos con su textura tierna y su sabor suave y ahumado. Nuestro Jamón Cocido Premium se elabora con las mejores piezas de carne de cerdo, cuidadosamente seleccionadas y cocidas lentamente para garantizar una textura jugosa y un sabor excepcional en cada rebanada. Su aroma suave y tentador es una invitación irresistible a disfrutar de una experiencia gastronómica auténtica y reconfortante. Cada rebanada de nuestro Jamón Cocido Premium es una muestra de calidad y artesanía.'
+    jamon.style.fontSize = '30px'
+    jamon.style.textAlign = "center"
+})
+
+
+
+let jamonIberico = document.getElementById("iberico");
+
+jamonIberico.addEventListener("click", function () {
+    jamonIberico.textContent = 'Déjate envolver por el sabor exquisito y la tradición centenaria con nuestro Jamón Ibérico de Bellota, un verdadero tesoro gastronómico que captura la esencia de la cultura culinaria española. Proveniente de cerdos ibéricos criados en libertad y alimentados exclusivamente con bellotas durante la montanera, este jamón es una obra maestra de la gastronomía que deleita los sentidos con su sabor único y su textura delicada. Cada loncha de nuestro Jamón Ibérico de Bellota es una experiencia sensorial que te transporta a los campos de encinas y alcornoques del suroeste de España, donde los cerdos ibéricos se alimentan de bellotas.'
+    jamonIberico.style.fontSize = '30px'
+    jamonIberico.style.textAlign = "center"
+
+})
+
+
+
+let salameColonial = document.getElementById("salame");
+
+salame.addEventListener("click", function () {
+    salame.textContent = 'Disfruta de la rica tradición y el sabor inconfundible con nuestro Salame Colonia Argentino, un clásico de la gastronomía argentina que deleita los sentidos con su aroma robusto y su sabor auténtico. El Salame Colonia es un embutido popular que se ha convertido en un elemento indispensable en la mesa de los argentinos, conocido por su calidad y su exquisito sabor. El Salame Colonia Argentino se caracteriza por su mezcla equilibrada de carnes magras de cerdo, condimentadas con ajo, pimienta negra y otras especias, que le confieren su sabor único y su aroma inconfundible.'
+    salame.style.fontSize = '30px'
+    salameColonial.style.textAlign = "center"
+
+})
+
+
+
+let quesoGruyere = document.getElementById("gruyere");
+
+quesoGruyere.addEventListener("click", function () {
+    quesoGruyere.textContent = 'Deléitate con la sofisticación y el sabor inigualable de nuestro queso Gruyère, una verdadera joya culinaria que encarna la elegancia y la tradición suiza en cada bocado. Proveniente de la región de Gruyères en Suiza, este queso es conocido por su sabor rico y complejo, así como por su textura suave y cremosa. El queso Gruyère se elabora con leche de vaca cruda y se somete a un proceso de maduración cuidadosamente controlado, que puede durar desde unos pocos meses hasta varios años. Durante este tiempo, el queso desarrolla su distintivo sabor a nuez y su textura firme pero tierna, creando una experiencia de degustación única y gratificante.'
+    quesoGruyere.style.fontSize = '30px'
+    quesoGruyere.style.textAlign = "center"
+
+})
+
+
+
+let quesoAzul = document.getElementById("azul");
+
+quesoAzul.addEventListener("click", function () {
+    quesoAzul.textContent = 'Adéntrate en la riqueza y complejidad del queso azul, una experiencia gastronómica que cautiva los sentidos con su sabor único y su textura inconfundible. Reconocido por su distintivo enmohecimiento y su pronunciado perfil de sabor, el queso azul es apreciado en todo el mundo por su audaz carácter y su versatilidad culinaria. Nuestro queso azul se elabora a partir de leche de vaca, cabra u oveja y se inocula con cultivos de moho Penicillium, que crean las vetas azules características en su interior. Durante el proceso de maduración, el queso desarrolla su sabor intenso y complejo.'
+    quesoAzul.style.fontSize = '30px'
+    quesoAzul.style.textAlign = "center"
+
+})
+
+
+
+let quesoBrie = document.getElementById("brie");
+
+quesoBrie.addEventListener("click", function () {
+    quesoBrie.textContent = 'Disfruta de la elegancia y el sabor sublime del queso Brie, una verdadera delicia culinaria que seduce los sentidos con su textura suave y su sabor delicado. Originario de Francia, este queso cremoso es apreciado en todo el mundo por su refinamiento y versatilidad en la cocina. El queso Brie se caracteriza por su corteza blanca y aterciopelada, que encierra un interior cremoso y untuoso. Elaborado con leche de vaca, el Brie madura lentamente para desarrollar su sabor delicado y ligeramente afrutado, con notas sutiles de nuez que lo hacen irresistible. Cada porción de nuestro queso Brie es una experiencia gastronómica que deleita los sentidos.'
+    quesoBrie.style.fontSize = '30px'
+    quesoBrie.style.textAlign = "center"
+
+})
+
+
